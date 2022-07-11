707cdfb62ffd511331b09d0e4ec0ca2d212ebc2b
jwson-automation <sonjungwoo9@gmail.com>
2022-07-11 20:43:55 +0900

    update

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-// java script는 자동적으로 html 코트를 읽어온다.
-const title = document.querySelector("div.hello:first-child h1");
+const title = document.querySelector(".hello:first-child h1");
 
-console.log(title);
+function handleTitleClick(){
+    console.log("title was clicked!");
+}
 
-title.addEventListener
+title.style.color ="blue"
+
+
+title.addEventListener("click", handleTitleClick)
